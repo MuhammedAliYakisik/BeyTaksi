a3adff5e17307af6f3a3de149c3723474f798646
Muhammed Ali <muhammedali8799@gmail.com>
2024-08-05 17:05:33 +0300

    color class done

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -4,10 +4,19 @@ import 'dart:ui';
 //Colors
 const Color bluee = Color(0xff195073);
 const Color red = Color(0xffF23207);
-const Color whiteblue = Color(0xff96D2D9);
+const Color blue1 = Color(0xff96D2D9);
 const Color orange = Color(0xffF27B35);
 const Color blackred = Color(0xff732509);
 const Color white = Color(0xffF0F0EC);
 const Color grey = Color(0x4A4AECC1);
-
+const Color blue2 = Color(0xffC9DFF2);
+const Color whiteyellow = Color(0xffFED791);
+const Color orange2 = Color(0xffD98B2B);
+const Color brown = Color(0xff8C521F);
+const Color whitebrown = Color(0xffA66D60);
+const Color blue3 = Color(0xff84B1D9);
+const Color yellow = Color(0xffF2E205);
+const Color yellow2 = Color(0xffF2CB05);
+const Color yellow3 = Color(0xffF2B705);
+const Color yellow4 = Color(0xffF2BC79);
 
