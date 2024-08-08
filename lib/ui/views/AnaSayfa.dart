c2d0f7bccabb0103a2b62b72e87a73b490a4e860
Muhammed Ali <muhammedali8799@gmail.com>
2024-08-08 03:48:10 +0300

    AnaSayfa Updated 1.0.1(bottomnavigationbar added)

--- lib/ui/views/AnaSayfa.dart
+++ lib/ui/views/AnaSayfa.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:izmir_taksi/ui/views/SearchPage.dart';
+import 'package:izmir_taksi/ui/views/SettingsPage.dart';
+import 'package:izmir_taksi/ui/views/SplashScreen.dart';
+import 'package:izmir_taksi/utils/color.dart';
+import 'package:motion_tab_bar/MotionBadgeWidget.dart';
+import 'package:motion_tab_bar/MotionTabBar.dart';
+import 'package:motion_tab_bar/MotionTabBarController.dart';
 
 class Anasayfa extends StatefulWidget {
   const Anasayfa({super.key});
@@ -7,9 +15,117 @@ class Anasayfa extends StatefulWidget {
   State<Anasayfa> createState() => _AnasayfaState();
 }
 
-class _AnasayfaState extends State<Anasayfa> {
+class _AnasayfaState extends State<Anasayfa> with TickerProviderStateMixin {
+  MotionTabBarController? _motionTabBarController;
+
+  @override
+  void initState() {
+    super.initState();
+    _motionTabBarController = MotionTabBarController(
+      initialIndex: 0,
+      length: 3,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _motionTabBarController!.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var oran = MediaQuery.of(context);
+    var genislik = oran.size.width;
+    var uzunluk = oran.size.height;
+
+    return Scaffold(
+      backgroundColor: white,
+      appBar: AppBar(
+        title: Text(
+          "~ İzmir Taksi ~",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: genislik / 15,
+          ),
+        ),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        leading: Padding(
+          padding: EdgeInsets.only(left: genislik / 35),
+          child: Image.asset("assets/taxi.png"),
+        ),
+      ),
+      bottomNavigationBar: MotionTabBar(
+        controller: _motionTabBarController, // ADD THIS if you need to change your tab programmatically
+        initialSelectedTab: "Home",
+        labels: const ["Home", "Search", "Settings"], // Dashboard'ı kaldır
+        icons: const [Icons.home, Icons.search, Icons.settings], // Dashboard'ı kaldır
+
+        // optional badges, length must be same with labels
+        badges: [
+          // Default Motion Badge Widget
+          const MotionBadgeWidget(
+            text: '10+',
+            textColor: Colors.white, // optional, default to Colors.white
+            color: Colors.black, // optional, default to Colors.red
+            size: 18, // optional, default to 18
+          ),
+
+          // custom badge Widget
+          Container(
+            color: Colors.black,
+            padding: const EdgeInsets.all(2),
+            child: const Text(
+              '+10',
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+              ),
+            ),
+          ),
+
+          // Default Motion Badge Widget with indicator only
+          const MotionBadgeWidget(
+            isIndicator: true,
+            color: yellow, // optional, default to Colors.red
+            size: 5, // optional, default to 5,
+            show: true, // true / false
+          ),
+        ],
+        tabSize: 50,
+        tabBarHeight: 55,
+        textStyle: const TextStyle(
+          fontSize: 12,
+          color: Colors.white,
+          fontWeight: FontWeight.w500,
+        ),
+        tabIconColor: yellow2,
+        tabIconSize: 28.0,
+        tabIconSelectedSize: 26.0,
+        tabSelectedColor: yellow,
+        tabIconSelectedColor: Colors.black,
+        tabBarColor: Colors.black,
+        onTabItemSelected: (int value) {
+          setState(() {
+            _motionTabBarController!.index = value;
+          });
+        },
+      ),
+      body: TabBarView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: _motionTabBarController,
+        children: <Widget>[
+          const Center(
+            child: Text("Home"),
+          ),
+          const Searchpage(),
+          const Settingspage(),
+        ],
+      ),
+    );
   }
 }
