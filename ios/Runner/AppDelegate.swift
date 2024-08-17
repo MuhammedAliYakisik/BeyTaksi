6ff385bee8954a46636744ef2cc14ea10928f6c3
Muhammed Ali <muhammedali8799@gmail.com>
2024-08-18 01:51:15 +0300

    SearchPage Updated 1.0.1 (map added)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBW8Q_eAnt_G0WIqLMwakwFwtqgNaWWULM")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/ui/views/SearchPage.dart
+++ lib/ui/views/SearchPage.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:izmir_taksi/utils/color.dart';
 
 class Searchpage extends StatefulWidget {
@@ -10,18 +14,62 @@ class Searchpage extends StatefulWidget {
 }
 
 class _SearchpageState extends State<Searchpage> {
+  //AIzaSyBW8Q_eAnt_G0WIqLMwakwFwtqgNaWWULM
+  Completer<GoogleMapController> haritakontrol = Completer();
+  var konum = CameraPosition(target: LatLng(38.123,26.213),zoom: 4);
+
+  List<Marker> isaret = <Marker>[];
+
+  Future<void> konumagit() async {
+    GoogleMapController controller = await haritakontrol.future;
+    var gidilecekkonum = CameraPosition(target: LatLng(38.41885,27.12872),zoom: 13);
+
+    var isaretduurm = Marker(
+      markerId: MarkerId("ID"),
+      position: LatLng(38.41885,27.12872),
+      infoWindow: InfoWindow(title: "Konum",snippet: "Şuanki Konumunuz"));
+    setState(() {
+      isaret.add(isaretduurm);
+    });
+    controller.animateCamera(CameraUpdate.newCameraPosition(gidilecekkonum));
+
+
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    konumagit();
+  }
+
   @override
   Widget build(BuildContext context) {
     var oran = MediaQuery.of(context);
     var genislik = oran.size.width;
     var uzunluk = oran.size.height;
+
     return Scaffold(
       backgroundColor: white,
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-           Text("google harita gelecek ve izmir şehrinden herhangi bir konumda bulunulacak")
+          children: <Widget>[
+
+            SizedBox(
+              height: 600,
+              width: 500,
+              child: GoogleMap(
+                mapType: MapType.normal,
+                markers: Set<Marker>.of(isaret),
+                initialCameraPosition: konum,
+                onMapCreated: (GoogleMapController controller){
+                  haritakontrol.complete(controller);
+                }
+              ),
+            )
+
+
+
           ],
         ),
       ),
