84842686a367ab96bfd2a59322ceba6386333d74
Muhammed Ali <muhammedali8799@gmail.com>
2024-08-18 17:56:10 +0300

    Taksi Model Class Updated 1.0.1

--- lib/data/model/Taksi.dart
+++ lib/data/model/Taksi.dart
@@ -1,52 +1,56 @@
 class Taksi {
-  String? iLCE;
-  String? kAPINO;
-  double? eNLEM;
-  String? aCIKLAMA;
-  String? iLCEID;
-  String? mAHALLE;
-  Null? mAHALLEID;
-  String? aDI;
-  double? bOYLAM;
-  String? yOL;
+  String? id;
+  String? qrCode;
+  String? categoryId;
+  String? categoryName;
+  String? title;
+  String? seotitle;
+  String? description;
+  String? image;
+  String? lat;
+  String? lng;
+  String? rate;
 
   Taksi(
-      {this.iLCE,
-        this.kAPINO,
-        this.eNLEM,
-        this.aCIKLAMA,
-        this.iLCEID,
-        this.mAHALLE,
-        this.mAHALLEID,
-        this.aDI,
-        this.bOYLAM,
-        this.yOL});
+      {this.id,
+        this.qrCode,
+        this.categoryId,
+        this.categoryName,
+        this.title,
+        this.seotitle,
+        this.description,
+        this.image,
+        this.lat,
+        this.lng,
+        this.rate});
 
   Taksi.fromJson(Map<String, dynamic> json) {
-    iLCE = json['ILCE'];
-    kAPINO = json['KAPINO'];
-    eNLEM = json['ENLEM'];
-    aCIKLAMA = json['ACIKLAMA'];
-    iLCEID = json['ILCEID'];
-    mAHALLE = json['MAHALLE'];
-    mAHALLEID = json['MAHALLEID'];
-    aDI = json['ADI'];
-    bOYLAM = json['BOYLAM'];
-    yOL = json['YOL'];
+    id = json['id'];
+    qrCode = json['qr_code'];
+    categoryId = json['category_id'];
+    categoryName = json['category_name'];
+    title = json['title'];
+    seotitle = json['seotitle'];
+    description = json['description'];
+    image = json['image'];
+    lat = json['lat'];
+    lng = json['lng'];
+    rate = json['rate'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['ILCE'] = this.iLCE;
-    data['KAPINO'] = this.kAPINO;
-    data['ENLEM'] = this.eNLEM;
-    data['ACIKLAMA'] = this.aCIKLAMA;
-    data['ILCEID'] = this.iLCEID;
-    data['MAHALLE'] = this.mAHALLE;
-    data['MAHALLEID'] = this.mAHALLEID;
-    data['ADI'] = this.aDI;
-    data['BOYLAM'] = this.bOYLAM;
-    data['YOL'] = this.yOL;
+    data['id'] = this.id;
+    data['qr_code'] = this.qrCode;
+    data['category_id'] = this.categoryId;
+    data['category_name'] = this.categoryName;
+    data['title'] = this.title;
+    data['seotitle'] = this.seotitle;
+    data['description'] = this.description;
+    data['image'] = this.image;
+    data['lat'] = this.lat;
+    data['lng'] = this.lng;
+    data['rate'] = this.rate;
     return data;
   }
 }
