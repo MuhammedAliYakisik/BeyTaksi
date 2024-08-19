0f2d14998a9e19ac053d316dc44bde25509aca16
Muhammed Ali <muhammedali8799@gmail.com>
2024-08-20 00:59:01 +0300

    Taksi Repo Updated 1.0.3 (map control added)

--- lib/data/repo/taksi_repo.dart
+++ lib/data/repo/taksi_repo.dart
@@ -3,32 +3,24 @@ import 'package:dio/dio.dart';
 import 'package:izmir_taksi/data/model/Taksi.dart';
 
 class TaksiRepo {
-  List<Taksi>? _items;
+  final _baseurl = "https://acikveri.konya.bel.tr/tr/dataset/932586d2-6229-4384-904b-fb72fefb81ef/resource/5b11fcef-a100-4842-8e72-9eba0566aa76/download/taksi_duraklari.json";
 
-  final _baseurl = "https://acikveri.beyoglu.bel.tr/api/c0358-d8851";
-
-  Future<List<Taksi>> fetchitems() async {
+  Future<Taksi> fetchTaksi() async {
     try {
       final response = await Dio().get(_baseurl);
-
       final data = response.data;
 
-      List<dynamic> jsonData;
-
       if (data is String) {
-        jsonData = json.decode(data);
-
-      } else if (data is List) {
-        jsonData = data;
-
+        final jsonData = json.decode(data);
+        return Taksi.fromJson(jsonData);
+      } else if (data is Map<String, dynamic>) {
+        return Taksi.fromJson(data);
       } else {
         throw Exception("Beklenmeyen veri formatı");
       }
-
-      _items = jsonData.map((e) => Taksi.fromJson(e)).toList();
     } catch (e) {
       print("Hata: $e");
+      throw e;
     }
-    return _items ?? [];
   }
 }
